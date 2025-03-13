5a51e0b878f8bc7d7c2782cacbaf999346910fad
FrancescaMiccolis <framic@unimore.it>
2025-03-13 11:28:06 +0100

    univ2 integration

--- models/builder.py
+++ models/builder.py
@@ -48,6 +48,35 @@ def get_encoder(model_name, target_img_size=224):
                             num_classes=0, 
                             dynamic_img_size=True)
         model.load_state_dict(torch.load(UNI_CKPT_PATH, map_location="cpu"), strict=True)
+    elif model_name == 'uni_v2':
+        HAS_UNI, UNI_CKPT_PATH = has_UNI()
+        assert HAS_UNI, 'UNIv2 is not available'
+        if not os.path.isfile(UNI_CKPT_PATH):
+            from huggingface_hub import login, hf_hub_download
+            login() # login with your User Access Token, found at https://huggingface.co/settings/tokens
+            os.makedirs(os.path.dirname(UNI_CKPT_PATH), exist_ok=True)
+            hf_hub_download('MahmoodLab/UNI2-h', filename="pytorch_model.bin", local_dir=ckpt_dir, force_download=True)
+
+        uni_kwargs = {
+            'model_name': 'vit_giant_patch14_224',
+            'img_size': 224, 
+            'patch_size': 14, 
+            'depth': 24,
+            'num_heads': 24,
+            'init_values': 1e-5, 
+            'embed_dim': 1536,
+            'mlp_ratio': 2.66667*2,
+            'num_classes': 0, 
+            'no_embed_class': True,
+            'mlp_layer': timm.layers.SwiGLUPacked, 
+            'act_layer': torch.nn.SiLU, 
+            'reg_tokens': 8, 
+            'dynamic_img_size': True
+        }
+        model = timm.create_model(**uni_kwargs)
+        state_dict = torch.load(UNI_CKPT_PATH, map_location="cpu")
+        keys = model.load_state_dict(state_dict, strict=True),
+        
     elif model_name == 'conch_v1':
         HAS_CONCH, CONCH_CKPT_PATH = has_CONCH()
         assert HAS_CONCH, 'CONCH is not available'
